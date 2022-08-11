0e9e7852c5fe5b0e3ed0dd54f2b1b7e492e46d40
Jeremiah Brockett <98862284+Jeremiah-81@users.noreply.github.com>
2022-08-11 11:44:30 -0700

    Updated reactions.js

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,35 +1,37 @@
 // Here we will use mongoose to get the data we are requesting.
-const { Schema, } = require('mongoose');
+const { Schema } = require("mongoose");
 
 // This is where the data we are requesting will be read from.
 const ReactionsSchema = new Schema(
-    {
-// This is where we will create a custom ID for each user.
+  {
+    // This is where we will create a custom ID for each user.
     reactionId: {
-        type: Schema.Types.ObjectId,
-        default: ()=> new Types.ObjectId()
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
     reactionBody: {
-       type: String,
-       required: true,
-       maxlength: 280
+      type: String,
+      required: true,
+      maxlength: 280,
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
- // This is where we will create and use our timestamp we made using the framework wrapper moment.js.
+    // This is where we will create and use our timestamp we made using the framework wrapper moment.js.
     createdAt: {
-        type: Date,
-        default: Date.now,
-    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-   }
-   },
-   {
-   toJSON: {
-   getters: true
-   } 
-   });
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 // This will tell node.js which bits of code to export from a given file so the other files are allowed access to the exported code.
 module.exports = Reactions;
